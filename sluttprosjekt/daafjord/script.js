bbd9b958a462fabc90011ca995101180b078261e
akselwe <aksel.wedsetlilleng1@gmail.com>
2023-04-13 23:05:08 +0200

    13.04.2024

--- sluttprosjekt/daafjord/script.js
+++ sluttprosjekt/daafjord/script.js
@@ -32,3 +32,18 @@ window.onscroll = () => {
 }
 
 
+/* isIntersecting er en api man kan bruke for å sjekke om noe på siden er på skjermen eller ikke */
+
+//observerer er et objekt, som tar inn en callbackfunc i konstruktøren
+const observerer = new IntersectionObserver((entries) => {
+    // for each er en lettere metode å loope gjennom -- entry blir hvert 
+    entries.forEach((entry)=>{
+        if (entry.isIntersecting){
+            entry.target.classList.add('show')
+        }
+        //else{entry.target.classList.remove('show')}
+    })
+});
+
+let hiddenEl = document.querySelectorAll('.hidden');
+hiddenEl.forEach((el) => observerer.observe(el));
---
+++ sluttprosjekt/java/snake/java.js
@@ -0,0 +1,181 @@
+
+let restartEl = document.getElementById("r")
+let scoreEl = document.getElementById("score")
+let highscoreEl = document.getElementById("highscore")
+
+restartEl.addEventListener("click", restart)
+function restart(){
+    window.location.reload()
+}
+
+// SNAKE Canvas JS, omtrent likt som pygame
+var blockSize = 25; 
+var cols = 20; // y-retning 
+var rows = 20; // x-retning
+
+//board til å tegne på med matte 
+var board; 
+// til å tegne med senere 
+var context; 
+
+// hodet start
+var snakeX = blockSize * 5;
+var snakeY =  blockSize * 5;
+
+// snake kropp
+var snakeKropp = [];
+
+// mat start 
+var matX;
+var matY;
+
+//fart slange 
+var vx = 0;
+var vy = 0;
+
+// score teller 
+var score = 0;
+
+var gameOver = false;
+
+// når siden loader, kjør følgende funksjon, som run løkken i pygame
+function load(){
+    board = document.getElementById("board");
+    board.height = rows * blockSize;
+    board.width = cols * blockSize;
+    //endre scoreEl sin width 
+    scoreEl.style.width = `${ cols * blockSize}px`
+    context = board.getContext("2d"); //for å tegne på brettet, 2 er ... 
+    highscoreEl.innerHTML = `Highscore: ${localStorage.highscore}`
+    
+    matPos();
+    document.addEventListener("keydown", endreRetning);
+    setInterval(update, 1000/10); // 100 ms
+
+}
+load()
+
+
+
+function endreRetning(e){
+    //objekt.code er som key.pressed i pygame
+    if (e.code == "ArrowUp" && vy != 1){
+        vx = 0;
+        vy = -1; 
+    }
+    else if (e.code == "ArrowDown" && vy != -1){
+        vx = 0;
+        vy = 1;
+    }
+    else if (e.code == "ArrowLeft" && vx != 1){
+        vx = -1; 
+        vy = 0;
+    }
+    else if (e.code == "ArrowRight" && vx != -1){
+        vx = 1;
+        vy = 0;
+    }
+    else if (e.code == "Space"){
+        console.log("space")
+        load()
+    }
+}
+
+function update(){
+    //board tegn 
+    context.fillStyle = "black";
+    context.fillRect(0,0,board.width, board.height); // samme som pygame. for å lage et svart lærret 
+
+    // mat tegn 
+    context.fillStyle = "red";
+    context.fillRect(matX,matY,blockSize, blockSize); 
+
+
+    if (snakeX == matX && snakeY == matY){
+
+        if(snakeKropp.length == 0) snakeKropp.push([snakeX, snakeY])
+        
+        else{
+            snakeKropp.push([snakeKropp[snakeKropp.length - 1][0], snakeKropp[snakeKropp.length - 1][1]])
+        }
+       
+        matPos()
+        score += 1
+        scoreEl.innerHTML = `Score: ${score}`
+    }
+
+    //snake tegn
+    context.fillStyle = "#d3ad7f";
+    snakeX += vx * blockSize;
+    snakeY += vy * blockSize;
+    context.fillRect(snakeX,snakeY,blockSize, blockSize);
+
+    for (let i = 0; i < snakeKropp.length; i++){
+        context.fillStyle ="#d3ad7f";
+        context.fillRect(snakeKropp[i][0], snakeKropp[i][1], blockSize, blockSize);
+    }
+
+    //halen skal ta igjen kroppen, for at brikkene skal vite hvor de skal
+    for (let i = snakeKropp.length-1; i > 0; i --){
+        snakeKropp[i] = snakeKropp[i-1];
+    }
+
+    //hvis slangen har en kropp, det etter hodet (snakeKropp[0]) lik x og y kordinatet til hodet
+    if (snakeKropp.length){
+        snakeKropp[0] = [snakeX, snakeY];
+    }
+
+   //gameOver om veggtreff eller om du går inn i deg selv 
+   if (snakeX < 0 || snakeX + blockSize > cols * blockSize || snakeY < 0 || snakeY +  blockSize > rows * blockSize){
+    gameOver = true; 
+   }
+   // sjekke om hodet har gått inn i kroppen, da erru dau
+   for (let i = 1; i < snakeKropp.length; i ++){
+    if (snakeX == snakeKropp[i][0] && snakeY ==snakeKropp[i][1]){
+        gameOver = true
+        console.log(score)
+    }
+   }
+
+
+    if (!localStorage.highscore){
+        localStorage.highscore = 0
+    }
+    if (score > Number(localStorage.highscore)){
+        localStorage.highscore = score
+        highscoreEl.innerHTML = `SCORE to BEAT: ${localStorage.highscore}`
+    } 
+   end()
+}
+
+function end(){
+    if(gameOver){
+        gameOverScreen()
+        backgroundDynam()
+    }
+}
+
+function gameOverScreen(){
+    //fyll skjermen svart
+    context.fillStyle = "#000000";
+    context.fillRect(0,0,board.width,board.height)
+    //game over 
+    context.font = "50px Arial";
+    context.fillStyle = "#FFFFFF"
+    context.fillText("GAME OVER", board.width/2 - 150, board.height/2)
+    context.font = "30px Arial";
+    context.fillStyle = "#FFFFFF"
+    context.fillText("Your score: " + score, board.width/2 - 150, board.height/2 + 50)
+    
+   }
+function backgroundDynam(){
+    document.body.style.backgroundColor = "#d3ad7f";
+}
+
+function matPos(){
+    // random(0,1) * 20 -> (0,19.999) * floor, for å få 19 og ikke 20 (tegner utfor) -> (0,19) * str på block = random start punkt
+    matX = Math.floor(Math.random(0,1) * cols) * blockSize;
+    matY = Math.floor(Math.random(0,1) * rows) * blockSize;
+}
+
+
